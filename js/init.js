ab2df2791b4e5ecd87da4bcc085b4b00a6d7f512
Hemanth Babu Parupalli <hemanthbabuparupalli@Hemanths-MacBook-Pro.local>
2020-04-29 12:55:47 -0300

    Complete project done

---
+++ js/init.js
@@ -0,0 +1,12 @@
+
+
+  $(document).ready(function(){
+    $('.sidenav').sidenav();
+    $('.myreviews').carousel({
+
+        numVisible :7,
+        shift: 55,
+        padding: 55
+
+    });
+  });
